63e109d6ff7552413db08993f00173a1cd0a57d6
lbouzoubaa <layla.bouzoubaa@med.miami.edu>
2019-09-03 16:01:03 -0400

    added rest of schedule

--- components/Footer.js
+++ components/Footer.js
@@ -6,9 +6,9 @@ import GDGLogo from './icons/gdg_logo.svg'
 export default () => (
   <Section skyline>
     <Lead>DevFest Miami 2019 is organized by</Lead>
-    <h1 style={{ fontSize: '2.5em' }}><GDGLogo alt="GDG" width="110px" style={{ position: 'relative', top: '11px' }} /> GDG Cloud Miami</h1>
+    <h1 style={{ fontSize: '2.5em' }}><GDGLogo alt="GDG" width="110px" style={{ position: 'relative', top: '11px' }} /> GDG Cloud Miami & GDG Miami</h1>
     <p className="cta">
-      <AnchorButton href="https://www.meetup.com/GDG-Cloud-Miami/" target="_blank" rel="noopener noreferrer" style={{ marginRight: '20px' }}>Register now</AnchorButton>
+      <AnchorButton href="https://www.eventbrite.com/e/devfest-miami-2019-tickets-68635601933?aff=eac2" target="_blank" rel="noopener noreferrer" style={{ marginRight: '20px' }}>Register now</AnchorButton>
       <AnchorButton color="red" href="https://www.meetup.com/GDG-Cloud-Miami/" target="_blank" rel="noopener noreferrer">Join us on Meetup</AnchorButton>
     </p>
   </Section>
