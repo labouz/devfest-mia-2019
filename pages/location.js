7871933d4cc6c7637f2e3d43b2761b8fa74f8043
César Díez Sánchez <cesaryomismo@gmail.com>
2019-09-25 23:11:31 -0400

    More content changes. Almost done with sponsors.

--- pages/location.js
+++ pages/location.js
@@ -11,11 +11,7 @@ export default () => (
     <Section>
       <PageTitle>Venue</PageTitle>
       <p>Wix.com Offices</p>
-      <h2>Travel &amp; Parking</h2>
-      <ul>
-        <li>Miami Beach Parking Garage on 17th Street: 640 17th St, Miami Beach, FL 33139</li>
-        <li>Lincoln Garage: 1691 Michigan Ave #206, Miami Beach, FL 33139</li>
-      </ul>
+      
       <br></br>
       <VenueMap />
     </Section>
